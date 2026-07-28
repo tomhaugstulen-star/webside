813d0eb272c83ac4a668346c3ac0f6a38ba2c28e
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-07-28 10:11:59 +0200

    feat: add editor type definitions

---
+++ src/types/editor.ts
@@ -0,0 +1,5 @@
+export type EditorTool = 'add' | 'pages' | 'layers' | 'assets'
+
+export type InspectorTool = 'design' | 'layout' | 'settings'
+
+export type ViewportMode = 'desktop' | 'tablet' | 'mobile'
