42fa292666174db473bdfa9609cb1560f9f0256d
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-07-29 20:07:45 +0200

    feat: require image asset data on creation

--- src/model/elementCreation.ts
+++ src/model/elementCreation.ts
@@ -1,11 +1,17 @@
 import type { ButtonAssetId } from './buttonAsset'
 import type { ElementKind } from './editorProject'
+import type { ImageAssetId, ImageAssetMetadata } from './imageAsset'
 
-export type StandardElementKind = Exclude<ElementKind, 'button'>
+export type StandardElementKind = Exclude<ElementKind, 'image' | 'button'>
 
 export type ElementCreationRequest =
   | {
       kind: StandardElementKind
+    }
+  | {
+      kind: 'image'
+      assetId: ImageAssetId
+      assetMetadata: ImageAssetMetadata
     }
   | {
       kind: 'button'
