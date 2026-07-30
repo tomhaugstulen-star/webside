5fbbcdcfa356df0fca61d264dbbc19e9d07ff2ac
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-07-30 21:05:42 +0200

    Decouple move preview from element kind

--- src/components/canvas/getPointerMovePreview.ts
+++ src/components/canvas/getPointerMovePreview.ts
@@ -1,18 +1,15 @@
 import type { ElementLayout } from '../../model/elementLayout'
-import type { EditorElement } from '../../model/editorProject'
 import type { AlignmentTargets } from './alignmentGuideTypes'
 import type { ElementLayoutPreview } from './canvasLayoutPreview'
 import { snapElementMove } from './snapElementMove'
 
 type GetPointerMovePreviewOptions = {
-  element: EditorElement
   layout: ElementLayout
   targets: AlignmentTargets | null
   canvasWidth: number
 }
 
 export function getPointerMovePreview({
-  element,
   layout,
   targets,
   canvasWidth,
@@ -22,12 +19,11 @@ export function getPointerMovePreview({
         layout,
         targets,
         canvasWidth,
-        allowHorizontal: element.kind !== 'header',
       })
     : { layout, guides: [] }
 
   return {
-    elementId: element.id,
+    elementId: '',
     layout: snapResult.layout,
     guides: snapResult.guides,
   }
