0f50e7689eab2940b8a8370da50f1f6beb2b95ce
tomhaugstulen-star <tom.haugstulen@gmail.com>
2026-07-30 05:22:30 +0200

    Remove header width model

--- src/model/headerWidth.ts
+++
@@ -1,9 +0,0 @@
-export const headerWidthModes = ['custom', 'full'] as const
-
-export type HeaderWidthMode = (typeof headerWidthModes)[number]
-
-export const DEFAULT_HEADER_WIDTH_MODE: HeaderWidthMode = 'custom'
-
-export function isHeaderWidthMode(value: unknown): value is HeaderWidthMode {
-  return headerWidthModes.includes(value as HeaderWidthMode)
-}
